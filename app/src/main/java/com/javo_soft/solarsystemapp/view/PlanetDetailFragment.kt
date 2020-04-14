221d8cdb6b90acaa602173be0c53498e50356a3e
Luis Javier Castro Reyes <luisjavier.castro@globant.com>
2020-04-14 14:24:14 -0500

    SSA-2/User_interface
    Refactor of packages.
    Addition of user interface:
    - Fragments
    - Item for the item list
    - Resources such as: string and style

---
+++ app/src/main/java/com/javo_soft/solarsystemapp/view/PlanetDetailFragment.kt
@@ -0,0 +1,24 @@
+package com.javo_soft.solarsystemapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.javo_soft.solarsystemapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PlanetDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_planet_detail, container, false)
+    }
+
+}
