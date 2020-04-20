d8caf927e23169fe655de171fca1e7f08c37b0d0
Luis Javier Castro Reyes <luisjavier.castro@globant.com>
2020-04-20 14:06:52 -0500

    SSA-3/Navigation_stuff_and_Model
    Refactor in Navigation structure (layouts)
    Addition of model

---
+++ app/src/main/java/com/javo_soft/solarsystemapp/model/Model.kt
@@ -0,0 +1,11 @@
+package com.javo_soft.solarsystemapp.model
+
+data class Planet(
+    var name: String?,
+    var description: String?,
+    var type: String?,
+    var orbitalPeriod: Int?,
+    var satellites: Int?,
+    var radius: Float?,
+    var imageUrl: String
+)
